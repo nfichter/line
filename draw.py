99836ffbb01329daa55eddbbba4c7ea92dc74322
Noah Fichter <nfichter@stuy.edu>
2017-02-14 11:55:18 -0500

    p much done

--- draw.py
+++ draw.py
@@ -18,51 +18,54 @@ def draw_line1(x0, y0, x1, y1, screen, color):
 			d+=B
 		x+=1
 		d+=A
+		color = adjust_color(color)
 		
 def draw_line2(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
-	A = 2*A
-	d = A+B
 	B = 2*B
+	d = A+B
+	A = 2*A
 	
 	x = x0
 	y = y0
 	
-	while x <= x1:
+	while y <= y1:
 		plot(screen,color,x,y)
 		if d < 0:
 			x+=1
 			d+=A
 		y+=1
 		d+=B
+		color = adjust_color(color)
 	
 def draw_line3(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
-	A = 2*A
-	d = A+B
 	B = 2*B
+	d = B-A
+	A = 2*A
 	
 	x = x0
 	y = y0
 	
-	while x >= x1:
+	while y <= y1:
 		plot(screen,color,x,y)
 		if d > 0:
 			x-=1
 			d-=A
 		y+=1
 		d+=B
+		color = adjust_color(color)
 
 def draw_line4(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
 	A = 2*A
-	d = A+B
+	d = B-A
 	B = 2*B
 	
 	x = x0
@@ -75,13 +78,14 @@ def draw_line4(x0, y0, x1, y1, screen, color):
 			d+=B
 		x-=1
 		d-=A
+		color = adjust_color(color)
 	
 def draw_line5(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
 	A = 2*A
-	d = A+B
+	d = 0-A-B
 	B = 2*B
 	
 	x = x0
@@ -94,51 +98,54 @@ def draw_line5(x0, y0, x1, y1, screen, color):
 			d-=B
 		x-=1
 		d-=A
+		color = adjust_color(color)
 	
 def draw_line6(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
-	A = 2*A
-	d = A+B
 	B = 2*B
+	d = 0-A-B
+	A = 2*A
 	
 	x = x0
 	y = y0
 	
-	while x >= x1:
+	while y >= y1:
 		plot(screen,color,x,y)
 		if d < 0:
 			x-=1
 			d-=A
 		y-=1
 		d-=B
+		color = adjust_color(color)
 
 def draw_line7(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
-	A = 2*A
-	d = A+B
 	B = 2*B
+	d = A-B
+	A = 2*A
 	
 	x = x0
 	y = y0
 	
-	while x <= x1:
+	while y >= y1:
 		plot(screen,color,x,y)
 		if d > 0:
 			x+=1
 			d+=A
 		y-=1
 		d-=B
+		color = adjust_color(color)
 		
 def draw_line8(x0, y0, x1, y1, screen, color):
 	A = y1-y0
 	B = -1*(x1-x0)
 	
 	A = 2*A
-	d = A+B
+	d = A-B
 	B = 2*B
 	
 	x = x0
@@ -151,6 +158,33 @@ def draw_line8(x0, y0, x1, y1, screen, color):
 			d-=B
 		x+=1
 		d+=A
+		color = adjust_color(color)
+		
+def adjust_color(color):
+	ret = [ 0, 0, 0 ]
+	ret[0] = color[0]
+	ret[1] = color[1]
+	ret[2] = color[2]
+	if color[0] >= 255:
+		ret[0] = 0
+		if color[1] >= 255:
+			ret[1] = 0
+			if color[2] >= 255:
+				ret[2] = 0
+			else:
+				ret[2]+=30
+				if ret[2] > 255:
+					ret[2] = 255
+		else:
+			ret[1]+=30
+			if ret[1] > 255:
+				ret[1] = 255
+	else:
+		ret[0]+=30
+		if ret[0] > 255:
+			ret[0] = 255
+	i = 0
+	return ret
 
 def draw_line_vert(x0, y0, x1, y1, screen, color):
 	y = y0
@@ -168,9 +202,6 @@ def draw_line_vert(x0, y0, x1, y1, screen, color):
 def draw_line(x0, y0, x1, y1, screen, color):
 	dx = x1-x0
 	dy = y1-y0
-	
-	if x0 == x1:
-		draw_line_vert(x0, y0, x1, y1, screen, color)
 	
 	if dx > 0:
 		if dy > 0:
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from display import *
 from draw import *
+import time
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 0, 0 ]
 
 #normal cases
 draw_line(250,250,450,350,screen,color)
@@ -14,6 +15,8 @@ draw_line(250,250,150,50,screen,color)
 draw_line(250,250,350,50,screen,color)
 draw_line(250,250,450,150,screen,color)
 
+color = [0,255,0]
+
 #edge cases
 draw_line(250,250,450,250,screen,color)
 draw_line(250,250,450,450,screen,color)
@@ -23,5 +26,15 @@ draw_line(250,250,50,250,screen,color)
 draw_line(250,250,50,50,screen,color)
 draw_line(250,250,250,50,screen,color)
 draw_line(250,250,450,50,screen,color)
+
+color = [0,0,255]
+
+#N
+draw_line(50,350,50,450,screen,color)
+draw_line(50,450,100,350,screen,color)
+draw_line(100,350,100,450,screen,color)
+
+#O
+draw_line(
 
 save_extension(screen, 'img.png')
